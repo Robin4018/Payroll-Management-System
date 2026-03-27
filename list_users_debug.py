2c88d2a9bc261eb40a69b0e180a84ff5c64feb10
Robin <robinchristy007@gmail.com>
2026-03-27 12:43:06 +0530

    Cleanup: Remove 100+ temporary development, debug, and utility scripts for a cleaner production-ready structure

--- list_users_debug.py
+++
@@ -1,4 +0,0 @@
-from django.contrib.auth.models import User
-print("Users count:", User.objects.count())
-for u in User.objects.all():
-    print(f"User: {u.username}, Super: {u.is_superuser}, Staff: {u.is_staff}")
--- run_server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Django Server...
-cd /d %~dp0
-"venv\Scripts\python.exe" manage.py runserver
-pause
